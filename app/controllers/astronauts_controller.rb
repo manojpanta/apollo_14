4fc076a57be51d029a15bbb4f04c1f5fa64ca866
manojpanta <manojpanta95@gmail.com>
2019-02-12 18:13:38 -0700

    implemented average age to the astronauts index page

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -2,6 +2,6 @@ class AstronautsController < ApplicationController
 
   def index
     @astronauts = Astronaut.all
+    @average_age = @astronauts.average_age.to_i
   end
-
 end
--- spec/features/visitor_can_see_list_of_astronauts_spec.rb
+++ spec/features/visitor_can_see_list_of_astronauts_spec.rb
@@ -16,4 +16,15 @@ describe 'visitor visiting astronauts index page' do
     expect(page).to have_content(astronaut2.age)
     expect(page).to have_content(astronaut2.job)
   end
+
+  it 'can show average age of all the astronauts' do
+    astronaut1 = Astronaut.create(name: 'astronaut1', age: 30, job: 'commandar')
+    astronaut2 = Astronaut.create(name: 'astronaut2', age: 25, job: 'leader')
+    astronaut3 = Astronaut.create(name: 'astronaut2', age: 35, job: 'leader')
+
+
+    visit '/astronauts'
+
+    expect(page).to have_content("Average Age: #{(astronaut1.age + astronaut2.age + astronaut3.age)/3}")
+  end
 end
